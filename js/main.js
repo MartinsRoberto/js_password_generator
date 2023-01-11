0276021f8ab830a7380576a76765d7aca0ac066c
MartinsRoberto <robertomartinsramm@gmail.com>
2023-01-11 16:24:42 -0300

    adicionado passwordstrength e mais estilos

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const lowercase = document.querySelector('#lowercase')
 const uppercase = document.querySelector('#uppercase')
 const symbols = document.querySelector('#symbols')
 const generatedPassword = document.querySelector('.password input')
+const tips = document.querySelector('.tips')
 
 let qtdOptions = []
 let password = ''
@@ -45,6 +46,9 @@ btnGenerate.addEventListener('click', () => {
   
   generatedPassword.value = password
 
+  tips.innerHTML = checkPasswordStrength(password)
+
+  passwordStrengthColor()
 })
 
 
@@ -65,3 +69,74 @@ const getSymbol = () => {
   return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+
+
+const checkPasswordStrength = (password) => {
+  // Initialize variables
+  var strength = 0;
+  var tips = "";
+
+  // Check password length
+  if (password.length < 8) {
+    tips += "Make the password longer. ";
+  } else {
+    strength += 1;
+  }
+
+  // Check for mixed case
+  if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
+    strength += 1;
+  } else {
+    tips += "Use both lowercase and uppercase letters. ";
+  }
+
+  // Check for numbers
+  if (password.match(/\d/)) {
+    strength += 1;
+  } else {
+    tips += "Include at least one number. ";
+  }
+
+  // Check for special characters
+  if (password.match(/[^a-zA-Z\d]/)) {
+    strength += 1;
+  } else {
+    tips += "Include at least one special character. ";
+  }
+
+  // Return results
+  if (strength < 2) {
+    return "Easy to guess. " + tips;
+  } else if (strength === 2) {
+    return "Medium difficulty. " + tips;
+  } else if (strength === 3) {
+    return "Difficult. " + tips;
+  } else {
+    return "Extremely difficult. " + tips;
+  }
+}
+
+const passwordStrengthColor = () => {
+  const bars = document.querySelectorAll('.strength-bar .bar')
+  
+  bars.forEach((bar) => bar.style.backgroundColor = "silver")
+
+  if(tips.innerText.includes('Easy')){
+    document.querySelector('.strength-bar .bar:nth-child(1)').style.backgroundColor = "#dc3545"
+  }
+  else if(tips.innerText.includes('Medium')){
+    document.querySelector('.strength-bar .bar:nth-child(1)').style.backgroundColor = "#ffc107"
+    document.querySelector('.strength-bar .bar:nth-child(2)').style.backgroundColor = "#ffc107"
+  }
+  else if(tips.innerText.includes('Difficult')){
+    document.querySelector('.strength-bar .bar:nth-child(1)').style.backgroundColor = "#0d6efd"
+    document.querySelector('.strength-bar .bar:nth-child(2)').style.backgroundColor = "#0d6efd"
+    document.querySelector('.strength-bar .bar:nth-child(3)').style.backgroundColor = "#0d6efd"
+  }
+  else if(tips.innerText.includes('Extremely difficult')){
+    document.querySelector('.strength-bar .bar:nth-child(1)').style.backgroundColor = "#198754"
+    document.querySelector('.strength-bar .bar:nth-child(2)').style.backgroundColor = "#198754"
+    document.querySelector('.strength-bar .bar:nth-child(3)').style.backgroundColor = "#198754"
+    document.querySelector('.strength-bar .bar:nth-child(4)').style.backgroundColor = "#198754"
+  }
+}
